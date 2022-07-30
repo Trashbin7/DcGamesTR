c2336f10174d635601c6350a315221c9954ddea4
unnamed-file <96432924+Trashbin7@users.noreply.github.com>
2022-07-30 21:17:38 +0300

    komutlar eklendi + token yenilendi

--- bot.py
+++ bot.py
@@ -2,49 +2,49 @@
 import os
 from colorama import Fore, Back, Style
 
-print("Hold on, importing important modules...")
+print("Bekleyin, önemli modüller yükleniyor...")
 
 os.system("cls")
 print("------------------------------------------------------")
-print("Welcome!")
+print("Merhaba!")
 print("")
-print(Fore.GREEN + "(!) Press [ENTER] to start the bot.")
+print(Fore.GREEN + "(!) Botu çalıştırmak için [ENTER] basın.")
 print(Style.RESET_ALL)
-print("github.com/trashbin7")
+print("github.com/trashbin7/DcGamesTR")
 print("------------------------------------------------------")
 input()
 os.system("cls")
 print("------------------------------------------------------")
-print("Loading bot...")
+print("Bot yükleniyor...")
 print()
 print(Fore.GREEN + "---" + Fore.RED + "-----------------")
 print(Style.RESET_ALL)
-print("importing...")
+print("Kuruluyor...")
 print("------------------------------------------------------")
 import discord
 from dotenv import load_dotenv
 import time
 os.system("cls")
 print("------------------------------------------------------")
-print("Loading bot...")
+print("Bot yükleniyor...")
 print()
 print(Fore.GREEN + "-----------" + Fore.RED + "---------")
 print(Style.RESET_ALL)
-print("Processing token...")
+print("Token yükleniyor...")
 print("------------------------------------------------------")
 load_dotenv()
-TOKEN = ('MTAwMjkwNzE0NTkzNjgzNDU5MA.GkY3F4.Rdzlhgyi6SYgyQScrU9Ylo7_fZqFAR4-z3dYAg')
+TOKEN = ('MTAwMjkwNzE0NTkzNjgzNDU5MA.GKpQHG.c4FnuxLgw8b975U822LT0EJxdV7lOJMK6VNzx4')
 GUILD = ('DISCORD_GUILD')
 
 client = discord.Client()
 
 os.system("cls")
 print("------------------------------------------------------")
-print("Loading bot...")
+print("Bot yükleniyor...")
 print()
 print(Fore.GREEN + "---------------" + Fore.RED + "-----")
 print(Style.RESET_ALL)
-print("Getting guild name...")
+print("Sunucu ismi alınıyor...")
 print("------------------------------------------------------")
 @client.event
 async def on_ready():
@@ -53,26 +53,53 @@ async def on_ready():
             break
     os.system("cls")
     print("------------------------------------------------------")
-    print("Loading bot...")
+    print("Bot yükleniyor...")
     print()
     print(Fore.GREEN + "--------------------")
     print(Style.RESET_ALL)
-    print("All Done!")
+    print("Tamamen hazır!")
     print("------------------------------------------------------")
     time.sleep(0.3)
     os.system("cls")
     print("------------------------------------------------------")
-    print(Fore.YELLOW + str(client.user) + Style.RESET_ALL + " is connected to the following guild:")
+    print(Fore.YELLOW + str(client.user) + Style.RESET_ALL + " Aşağıdaki sunucuya bağlı:")
     print(Fore.YELLOW + str(guild.name))
     print(Style.RESET_ALL + "(id: " + Fore.YELLOW + str(guild.id) + Style.RESET_ALL + ")")
     print()
-    print("github.com/trashbin7")
+    print("github.com/trashbin7/DcGamesTR")
     print("------------------------------------------------------")
 
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f"Merhaba {member.name}, Discord sunucumuza hoş geldin!")
+        f"Merhaba {member.name}, Discord sunucumuza hoş geldin! \n discord.gg/4w8D6yrKCX")
+
+@client.event
+async def on_member_remove(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Merhaba {member.name}, Discord sunucumuzdan ayrıldın! \n discord.gg/4w8D6yrKCX")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!ping'):
+        await message.channel.send('Pong!')
+
+@client.event
+async def on_member_ban(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Merhaba {member.name}, Discord sunucumuzdan **banlandın!** \n discord.gg/4w8D6yrKCX")
+
+@client.event
+async def on_member_unban(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Merhaba {member.name}, Discord sunucumuzdaki **banın kaldırıldı!** \n discord.gg/4w8D6yrKCX")
+
 
 client.run(TOKEN)
